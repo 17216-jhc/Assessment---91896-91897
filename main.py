52c626f5ac413a3c968e7b53c21828c3f61c1e95
Benjamin Scott <84690193+17216-jhc@users.noreply.github.com>
2022-03-23 01:38:56 +0000

    Made code more robust by adding definitions for repeated code, changed from using int to float to ensure user can use shapes with sides with decimal places

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import math
 
 # ____________   FUNCTIONS ________________
+
 def clear():
    print("\033[H\033[J", end="")
 
@@ -22,12 +23,19 @@ def shape_chooser():
     print()
 
 def in_values(side):
-  value=int(input(f"Enter {side}: "))
+  value=float(input(f"Enter {side}: "))
   return value
+
+def A(shape, final_area):
+  print(f"Area of {shape} : ",final_area)
+  
+def P(shape, final_perimeter):
+  print(f"Perimeter of {shape} : ",final_perimeter)
 
 def triangle():
   clear()
-  print("Triangle")
+  shape_chosen = "Triangle"
+  print(shape_chosen)
   b=in_values("Base")
   s1=in_values("Side 1")
   s2=in_values("Side 2")
@@ -36,26 +44,28 @@ def triangle():
   perimeter=b+s1+s2
   print()
   
-  print("Area of Triangle : ",area)
-  print("Perimeter of Triangle : ",perimeter)
+  A(shape_chosen, area)
+  P(shape_chosen, perimeter)
   print()
 
 def rectangle():
   clear()
-  print("Rectangle")
+  shape_chosen = "Rectangle"
+  print(shape_chosen)
   l=in_values("Length")
   w=in_values("Width")
   area=l*w
   perimeter=2*(l+w)
   print()
   
-  print("Area of Rectangle : ",area)
-  print("Perimeter of Rectangle : ",perimeter)
+  A(shape_chosen, area)
+  P(shape_chosen, perimeter)
   print()
   
 def parallel():
   clear()
-  print("Parallelogram")
+  shape_chosen = "Parallelogram"
+  print(shape_chosen)
   l=in_values("Length")
   w=in_values("Width")
   h=in_values("Height")
@@ -63,21 +73,22 @@ def parallel():
   perimeter=2*(l+w)
   print()
   
-  print("Area of Parallelogram : ",area)
-  print("Perimeter of Parallelogram : ",perimeter)
+  A(shape_chosen, area)
+  P(shape_chosen, perimeter)
   print()
 
 def circle():
   clear()
-  print("Circle")
+  shape_chosen = "Circle"
+  print(shape_chosen)
   r=in_values("Radius")
   d=r+r
   area=math.pi*r*r
   perimeter=math.pi*d
   print()
-  
-  print("Area of Circle : ",area)
-  print("Perimeter of Circle : ",perimeter)
+
+  A(shape_chosen, area)
+  P(shape_chosen, perimeter)
   print()
 
 # ____________   MAIN  ________________
